823654231730c662e342f8391d9bfb20c8251253
adrianchvzz0 <adrianchvzz@gmail.com>
2025-05-24 02:12:30 -0600

    actualización de diseño del botón primary

--- src/components/components/Button.jsx
+++ src/components/components/Button.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 function getButtonClass(variant = "default") {
     const base =
-        "px-4 py-2 rounded-md font-semibold shadow-md duration-500 hover:scale-105 transition";
+        "px-4 py-2 rounded-lg font-semibold font-sans shadow-md duration-300 hover:scale-105 transition mt-2 ";
 
     const variants = {
         default: `${base} bg-gray-200 text-black`,
-        primary: `${base} bg-PrimaryGreen text-white`,
+        primary: `${base} bg-gradient-to-r from-PrimaryGreen to-DarkGreen text-white`,
         danger: `${base} bg-red-500 text-white`,
         outline: `${base} border border-white text-white bg-transparent`,
         disabled: `${base} bg-gray-400 text-gray-100 cursor-not-allowed`,
